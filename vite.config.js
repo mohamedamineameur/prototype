7573af33961c7d266726682d713545e729e631c4
aminefaw89 <mohamed.amine.ameur1@gmail.com>
2025-07-23 10:09:14 -0400

    fix: correct formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/prototype/', // <-- important !,
-  
+  base: '/prototype/', // <-- important !
 });
