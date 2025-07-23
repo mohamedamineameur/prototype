b006991eaf11f06969b1dc360f2b3e7f84f7132d
aminefaw89 <mohamed.amine.ameur1@gmail.com>
2025-07-23 10:46:22 -0400

    fix: enhance layout for Inscription and Login forms with overflow handling

--- src/pages/Inscription.jsx
+++ src/pages/Inscription.jsx
@@ -44,8 +44,8 @@ const RegisterForm = () => {
   );
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#1D4E89] font-['Archivo'] px-4">
-      <div className="bg-white rounded-2xl shadow-lg w-full max-w-4xl flex flex-col md:flex-row overflow-hidden">
+<div className="min-h-screen bg-[#1D4E89] font-['Archivo'] px-4 flex items-center justify-center overflow-y-auto py-8">
+<div className="bg-white rounded-2xl shadow-lg w-full max-w-4xl flex flex-col md:flex-row overflow-hidden">
         {/* Section logo à gauche */}
         <div className="md:w-1/2 w-full flex items-center justify-center bg-white p-8 border-r md:border-r border-b md:border-b-0">
           <img src={`${BASE}symbole_couleur.png`} alt="Logo" className="w-32" />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#1D4E89] font-['Archivo'] px-4">
+    <div className="min-h-screen flex items-center justify-center bg-[#1D4E89] font-['Archivo'] overflow-y-auto  px-4">
       <div className="bg-white rounded-2xl shadow-lg w-full max-w-4xl flex flex-col md:flex-row overflow-hidden">
         {/* Section logo à gauche */}
         <div className="md:w-1/2 w-full flex items-center justify-center bg-white p-8 border-r md:border-r border-b md:border-b-0">
